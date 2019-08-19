0474aa9b71c061a477b6ec4208452c33230b124e
JohnnyLiu <13814020314@163.com>
2019-08-20 00:16:42 +0800

    添加了pages文件夹，同时引入了fastclick插件，border.css，reset.css

--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -1,15 +1,12 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+    <div>HomePage</div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'Home'
 }
 </script>
 
-<style>
-
+<style scoped>
 </style>
